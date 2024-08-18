25ccf326c416cf3e60bf934c8054ad9ddd0f4ec2
Salatiel Queiroz <salatielsqueiroz@gmail.com>
2024-08-18 05:57:03 -0300

    fix: remove umami script

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,15 +1,5 @@
 import React from "react";
-import { Script } from "gatsby";
 
 export const wrapPageElement = ({ element }) => {
-  return (
-    <>
-      {element}
-      <Script
-        defer
-        src="http://umami-qc8ksw8.82.112.245.222.sslip.io/script.js"
-        data-website-id="822db299-d1d7-4a58-9834-593aabef1a11"
-      />
-    </>
-  );
+  return <>{element}</>;
 };
